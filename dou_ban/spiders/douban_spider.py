16ad0ab30abef030cbc9223d982274300912098a
zm <2272598099@qq.com>
2019-02-17 20:56:02 +0800

    version 0.2

--- dou_ban/spiders/douban_spider.py
+++ dou_ban/spiders/douban_spider.py
@@ -16,29 +16,20 @@ from scrapy import Selector, Request
 class DoubanSpider(scrapy.Spider):
     name = "douban"
     allowed_domains = ["douban.com"]
-    start_urls = ['https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=%E7%94%B5%E5%BD%B1&start=0&year_range=2018,2018']
+    start_urls = ['https://movie.douban.com/subject/30145962/']
+    # start_urls = ['https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=%E7%94%B5%E5%BD%B1&start=0&year_range=2018,2018']
     base_url_prefix = 'https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=%E7%94%B5%E5%BD%B1&start='
     base_url_suffix = '&year_range=2018,2018'
 
     test = 0
-    #
-    # # 代理测试
-    # def start_requests(self):
-    #     url = 'https://movie.douban.com/tag/#/?sort=U&range=0,10&tags=%E7%94%B5%E5%BD%B1,2018'
-    #     for i in range(4):
-    #         yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
-    #
-    # def parse(self, response):
-    #     if response.text is not None:
-    #         print('返回成功！')
-    #         print()
 
     def parse(self, response):
         urls = []
         start = re.search(r'start=(\d*)', response.url).group(1)
         rs = json.loads(response.text)
         data = rs.get('data')
-        if len(data) != 0 and self.test < 1:
+        if len(data) != 0:
+            # and self.test < 1
             self.test += 1
             try:
                 yield Request(self.base_url_prefix + str(int(start) + 20) + self.base_url_suffix, callback=self.parse)
@@ -62,8 +53,8 @@ class DoubanSpider(scrapy.Spider):
         json_data = json.loads(selector.xpath('//script[@type="application/ld+json"]/text()').extract()[0].replace('\n', ''), strict=False)
         item['url'] = str(response.url)
         item['name'] = json_data.get('name')
-        temp = text.re(r'又名:(.*)\n')
-        item['also_known'] = '' if len(temp) == 0 else temp[0]
+        also_known = text.re(r'又名:(.*)\n')
+        item['also_known'] = '' if len(also_known) == 0 else also_known[0]
         item['movie_info'] = json_data.get('description')
         score = json_data.get('aggregateRating').get('ratingValue')
         if score == '':
@@ -71,16 +62,25 @@ class DoubanSpider(scrapy.Spider):
         else:
             item['score'] = float(score)
 
-        item['director'] = text.re(r'导演:(.*)\n')[0]
-        item['screenwriter'] = text.re(r'编剧:(.*)\n')[0]
+        screenwriter = text.re(r'编剧:(.*)\n')
+        director = text.re(r'导演:(.*)\n')
+        genre = text.re(r'类型:(.*)\n')
+        runtime = text.re(r'片长:(.*)\n')
+        country = text.re(r'地区:(.*)\n')
+        stars = text.re(r'主演:(.*)\n')
+        language = text.re(r'语言:(.*)\n')
+
+        item['director'] = '' if len(director) == 0 else director[0]
+        item['screenwriter'] = '' if len(screenwriter) == 0 else screenwriter[0]
         # item['stars'] = self.json_util(json_data.get('actor'))
-        item['stars'] = text.re(r'主演:(.*)\n')[0]
+        item['stars'] = '' if len(stars) == 0 else stars[0]
         # item['genre'] = ''.join([e for e in one_movie.xpath('.//span[@property="v:genre"]/text()').extract()])
-        item['genre'] = text.re(r'类型:(.*)\n')[0]
-        item['country'] = text.re(r'地区:(.*)\n')[0]
-        item['language'] = text.re(r'语言:(.*)\n')[0]
+
+        item['genre'] = '' if len(genre) == 0 else genre[0]
+        item['country'] = '' if len(country) == 0 else country[0]
+        item['language'] = '' if len(language) == 0 else language[0]
         item['release_date'] = json_data.get('datePublished')
-        item['runtime'] = text.re(r'片长:(.*)\n')[0]
+        item['runtime'] = '' if len(runtime) == 0 else runtime[0]
         # print(json.loads(json_data[0]).get('director'))
         # print(len(json_data))
         yield(item)
